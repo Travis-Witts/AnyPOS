2d5f5872c53095d92b86486ded676631bb07203f
Travis297 <74891603+Travis297@users.noreply.github.com>
2021-05-19 01:17:08 +0930

    :children_crossing: feat: added invoice to sales page :children_crossing:

--- client/src/components/Sale/index.tsx
+++ client/src/components/Sale/index.tsx
@@ -1,21 +1,52 @@
 import React from 'react';
-import { Button } from 'react-bulma-components';
+import { Form, Button } from 'react-bulma-components';
+import './style.scss';
 
-const arr: number[] = []
+const arr: number[] = [];
 
-for (let i=0; i< 20; i+=1) {
-  arr.push(i)
+for (let i = 0; i < 20; i += 1) {
+  arr.push(i);
 }
 const Sale: React.FC = () => (
   <div className="sale-container">
-    <div className="item-col">
-      <h1 className="items-header">Add an item:</h1>
-    {arr.map(button => <Button className="sale-btn" color="success" size="large"><p>Hello</p> <p>There</p></Button>)}
+    <div className="sale-col">
+      <h1 className="items-header">Add an item</h1>
+      <div className="item-col">
+        {arr.map(button => (
+          <Button
+            id={button.toString()}
+            className="sale-btn"
+            color="success"
+            size="large"
+          >
+            <p>Hello</p> <p>There</p>
+          </Button>
+        ))}
+      </div>
     </div>
     <div className="sales-row">
       <div className="invoice-col">
-        <div className="invoice-header">Invoice lines:</div>
+        <div className="invoice-header">
+          <h1>New Sale</h1>
+        </div>
         <div className="current-invoice" />
+        <div className="invoice-footer">
+        <div className="payment-row">
+              Total $
+              <Form.Input className="total-form" />
+              </div>
+          <div className="payment-row">
+              GST $
+              <Form.Input className="gst-form" />
+              </div>
+          <div className="payment-row">
+              <p>Discount %</p>
+              <Form.Input className="discount-form" />
+              </div>
+            <Button className="pay-btn" inverted color="success">
+              Pay
+            </Button>
+        </div>
       </div>
     </div>
   </div>
