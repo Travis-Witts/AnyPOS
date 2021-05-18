8c139cdd85ad126f946274d7481150eafab4ec9b
Travis297 <74891603+Travis297@users.noreply.github.com>
2021-05-18 16:44:37 +0930

    git Revert ":construction: feat: added sales page :construction:"
    This reverts commit 24f158bc9ff35ec1865f35f0255c89daa2b3d238.

--- client/src/components/Sale/index.tsx
+++ client/src/components/Sale/index.tsx
@@ -4,53 +4,20 @@ const Sale: React.FC = () => (
   <div className="sale-container">
     <div className="sales-row">
       <div className="invoice-col">
-        <div className="invoice-header">
-          <h1>Invoice lines:</h1>
-        </div>
+          <div className="invoice-header">Invoice lines:</div>
         <div className="current-invoice" />
-        <div className="invoice-footer">
-          <p># of Items: </p>
-          <p>Total: $</p>
-          <p>GST: </p>
-        </div>
+        <div className="invoice-footer">Total:</div>
       </div>
-      <div className="categories">
-        <h1>Category:</h1>
-        <div className="select">
-          <select name="slct" id="slct">
-            <option value="idk">idkasdkljdas</option>
-            <option value="idk">idk</option>
-            <option value="idk">idk</option>
-          </select>
-        </div>
-        <h1 id="item-text">Item:</h1>
-        <div className="select">
-          <select name="slct" id="slct">
-            <option value="idk">idkasdkljdas</option>
-            <option value="idk">idk</option>
-            <option value="idk">idk</option>
-          </select>
-        </div>
-        <h1 id="item-text">Discount:</h1>
-        <div className="select">
-          <select name="slct" id="slct">
-            <option value="idk">0%</option>
-            <option value="idk">5%</option>
-            <option value="idk">10%</option>
-          </select>
-        </div>
-        <h1 id="item-text">Excl. GST:</h1>
-        <div className="item-price">
-          <div>
-            $<span />
-          </div>
-        </div>
-        <h1 id="item-text">Incl. GST:</h1>
-        <div className="item-price">
-          <div>
-            $<span />
-          </div>
-        </div>
+      <div className="sales-btns">
+        <button type="submit" className="add">
+          Add
+        </button>
+        <button type="submit" className="add">
+          Add
+        </button>
+        <button type="submit" className="add">
+          Add
+        </button>
       </div>
     </div>
   </div>
