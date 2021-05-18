1a7f857ae3490ed065abac93d053f699f21bde8b
Travis297 <travis.witts@outlook.com>
2021-05-18 23:03:31 +0930

    :sparkles: feat: added sale buttons :sparkles:

--- client/src/components/Sale/index.tsx
+++ client/src/components/Sale/index.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import { Button } from 'react-bulma-components';
 
+const arr: number[] = []
+
+for (let i=0; i< 20; i+=1) {
+  arr.push(i)
+}
 const Sale: React.FC = () => (
   <div className="sale-container">
-    <div className="item-col" />
+    <div className="item-col">
+      <h1 className="items-header">Add an item:</h1>
+    {arr.map(button => <Button className="sale-btn" color="success" size="large"><p>Hello</p> <p>There</p></Button>)}
+    </div>
     <div className="sales-row">
       <div className="invoice-col">
-          <div className="invoice-header">Invoice lines:</div>
+        <div className="invoice-header">Invoice lines:</div>
         <div className="current-invoice" />
       </div>
     </div>
---
+++ client/src/components/SalesButton/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SalesButton: React.FC = () => (
+    <div/>
+);
+
+export default SalesButton;
