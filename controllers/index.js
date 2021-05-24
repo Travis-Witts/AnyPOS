daee5e344def80a9e9715318de9058247d760838
Travis297 <74891603+Travis297@users.noreply.github.com>
2021-05-24 17:13:31 +0930

    :file_folder: chore: created controllers :file_folder:

---
+++ controllers/index.js
@@ -0,0 +1,15 @@
+const UserController = require('./user.controller');
+const ProductController = require('./product.controller');
+const StockController = require('./stock.controller');
+const StockTransactionController = require('./stockTransaction.controller');
+const StoreController = require('./store.controller');
+const TransactionController = require('./transaction.controller')
+
+module.exports = {
+  UserController,
+  ProductController,
+  StockController,
+  StockTransactionController,
+  StoreController,
+  TransactionController,
+};
