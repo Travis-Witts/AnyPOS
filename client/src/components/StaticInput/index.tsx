cd09dbbbb9f58e804babcbd46d9744542de28153
Travis297 <74891603+Travis297@users.noreply.github.com>
2021-05-29 17:32:39 +0930

    :construction: feat: added static input component :construction:

---
+++ client/src/components/StaticInput/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+type IInputText = {
+  text: string;
+  label: string;
+  classes: string;
+};
+
+const StaticInput: React.FC<IInputText> = ({ text, label, classes }: IInputText) => (
+    <form>
+        <label htmlFor={label}>{label}:  $</label>
+          <input className={classes} id={label} type="text" value={text} disabled />
+    </form>
+
+);
+
+export default StaticInput;
