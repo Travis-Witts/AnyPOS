f0ec4c28a4c41228e365a0c5051b1896604397b7
Travis297 <74891603+Travis297@users.noreply.github.com>
2021-05-30 02:29:31 +0930

    :construction: feat: added edit store page

--- client/src/components/EditContainer/index.tsx
+++ client/src/components/EditContainer/index.tsx
@@ -1,22 +1,19 @@
-import React, { useRef, useState} from 'react';
+import React, { useRef, useState } from 'react';
 import './style.scss';
 import axios from 'axios';
+import ProductList from '../ProductList';
 
 type IEditProps = {
   storeId: string;
 };
 
 type IProduct = {
-  name: string | undefined,
-  price: number | undefined,
-  quantity: number | undefined
-}
+  name: string | undefined;
+  price: number | undefined;
+  quantity: number | undefined;
+};
 
 const EditContainer: React.FC<IEditProps> = ({ storeId }: IEditProps) => {
-  const [nameState, setName] = useState<string | undefined>('');
-  const [priceState, setPrice] = useState<string | undefined>('');
-  const [quantityState, setQuantity] = useState<string | undefined>('');
-  // Product references
   const productNameRef = useRef<HTMLDivElement>(null);
   const productPriceRef = useRef<HTMLDivElement>(null);
   const productQuantityRef = useRef<HTMLDivElement>(null);
@@ -25,38 +22,52 @@ const EditContainer: React.FC<IEditProps> = ({ storeId }: IEditProps) => {
     const productBody: IProduct = {
       name: productNameRef.current?.innerText,
       price: Number(productPriceRef.current?.innerText),
-      quantity: Number(productQuantityRef.current?.innerText)
-    }
-    await axios.post("/product", productBody)
+      quantity: Number(productQuantityRef.current?.innerText),
+    };
+    await axios.post('/product', productBody);
   };
   return (
     <div className="main">
-      <div className="product-container-row">
-        <div className="new-product-col">
-          {' '}
-          New Product:
-          <div className="product">
-            <h1>Name:</h1>{' '}
-            <div contentEditable="true" ref={productNameRef}>
-              {' '}
-              {nameState}{' '}
+      <div className="container">
+        <div className="product-container-row row">
+          <div className="new-product-col col-md-4">
+            {' '}
+            <h1 className="row">New Product:</h1>
+            <div className="product container">
+              <div className="row">
+                <h3>Name:</h3>{' '}
+                <div contentEditable="true" ref={productNameRef}>
+                  {' '}
+                </div>
+              </div>
+              <div className="row">
+                <h3>Price:</h3>{' '}
+                <div contentEditable="true" ref={productPriceRef}>
+                  0
+                </div>
+              </div>
+              <div className="row">
+                <h3>Quantity:</h3>{' '}
+                <div contentEditable="true" ref={productQuantityRef}>
+                  0
+                </div>
+              </div>
+              <div className="row">
+                <button
+                  onClick={newProductHandler}
+                  type="submit"
+                  className="new-product btn btn-outline-dark"
+                >
+                  Save
+                </button>
+              </div>
             </div>
-            <h1>Price:</h1>{' '}
-            <div contentEditable="true" ref={productPriceRef}>
-              0
-            </div>
-            <h1>Quantity:</h1>{' '}
-            <div contentEditable="true" ref={productQuantityRef}>
-              0
-            </div>
-            <button onClick={newProductHandler} type="submit" className="new-product">
-            Save
-          </button>
           </div>
-        </div>
-        <div className="product-list-col">
-          Product List:
-          <div className="product-list">Hello</div>
+          <div className="product-list-col col-md-4">
+
+            <h1 className="row">Product List: </h1>
+              <ProductList />
+          </div>
         </div>
       </div>
     </div>
