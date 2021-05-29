20f20f29aa8dbc63fe115249bc479bccddd0de99
Travis297 <74891603+Travis297@users.noreply.github.com>
2021-05-30 02:30:12 +0930

    :construction: feat: added Edit Product component

--- client/src/components/EditProduct/index.tsx
+++ client/src/components/EditProduct/index.tsx
@@ -1,13 +1,121 @@
-import React, { useRef } from 'react';
+import axios from 'axios';
+import React, { useRef, useState, useEffect } from 'react';
 import './style.scss';
 
-const EditProduct: React.FC = () => {
-    const productNameRef = useRef<HTMLDivElement>(null);
-    const productPriceRef = useRef<HTMLDivElement>(null);
-    const productQuantityRef = useRef<HTMLDivElement>(null);
-    return (
-        <div/>
-    )
-}
+type IProduct = {
+  name: string | undefined;
+  price: number | undefined;
+  quantity: number | undefined;
+  product_id: string | undefined;
+};
+
+type IDeleteBody = {
+  id: string | undefined;
+};
+
+const EditProduct: React.FC<IProduct> = ({
+  name,
+  price,
+  quantity,
+  product_id,
+}: IProduct) => {
+  const [priceState, setPrice] = useState<number | undefined>(0);
+  const [quantityState, setQuantity] = useState<number | undefined>(0);
+  // Product references
+  const productPriceRef = useRef<HTMLInputElement>(null);
+  const productQuantityRef = useRef<HTMLInputElement>(null);
+
+  const deleteHandler = async () => {
+    const config = {
+      data: {
+        id: product_id,
+      },
+    };
+    await axios.delete('/product', config);
+  };
+    const saveHandler = () => {
+      const productBody = {
+          id: product_id,
+
+      }
+
+    }
+
+  useEffect(() => {
+    setPrice(price);
+    setQuantity(quantity);
+  }, [price, quantity]);
+
+  return (
+    <li className="list-group-item row">
+      <div className="col">
+        <div className="row">
+          <div className="col">
+            <div className="row">
+              <div className="col">
+                <strong>Name:</strong>
+                <p>{name}</p>
+              </div>
+            </div>
+          </div>
+          <div className="col">
+            <div className="row">
+              <div className="col">
+                <strong>Quantity</strong>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col">
+                <input
+                  className="input-col"
+                  type="text"
+                  value={quantityState}
+                  ref={productQuantityRef}
+                />
+              </div>
+            </div>
+          </div>
+          <div className="col">
+            <div className="row">
+              <div className="col">
+                <strong>Price</strong>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col">
+                <input
+                  className="input-col"
+                  type="text"
+                  value={priceState}
+                  ref={productPriceRef}
+                />
+              </div>
+            </div>
+          </div>
+          <div className="col">
+            <div className="row">
+              <div className="col">
+              <button
+                  className="btn btn-outline-info"
+                  onClick={saveHandler}
+                  type="submit"
+                >
+                  Save
+                </button>
+                <button
+                  className="btn btn-outline-dark"
+                  onClick={deleteHandler}
+                  type="submit"
+                >
+                  Delete
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </li>
+  );
+};
 
 export default EditProduct;
