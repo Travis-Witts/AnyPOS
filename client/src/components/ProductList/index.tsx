73ea0eb82d6aa49145005780be57c6503624ed0c
Travis297 <74891603+Travis297@users.noreply.github.com>
2021-05-30 02:30:43 +0930

    :construction: feat: added product list item

--- client/src/components/ProductList/index.tsx
+++ client/src/components/ProductList/index.tsx
@@ -1,14 +1,45 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './style.scss';
+import axios from 'axios';
 import EditProduct from '../EditProduct';
 
-const ProductList: React.FC = () => {
-    const getProducts = async () => {
+type IProduct = {
+  name: string | undefined;
+  price: number | undefined;
+  quantity: number | undefined;
+  product_id: string | undefined;
+};
 
-    }
-    return (
-        <div />
-    )
-}
+const ProductList: React.FC = () => {
+  const [productsState, setProducts] = useState<IProduct[]>([]);
+
+  const getProducts = async () => {
+    const products = await axios.get('/product');
+    setProducts(products.data);
+    console.log(products.data)
+  };
+
+  useEffect(() => {
+    void getProducts();
+  }, []);
+  return (
+    <div>
+      {productsState.length ? (
+          <ul className="list-group">
+            {productsState.map((product: IProduct) => (
+              <EditProduct
+              product_id={product.product_id}
+                name={product.name}
+                price={product.price}
+                quantity={product.quantity}
+              />
+            ))}
+          </ul>
+      ) : (
+        <h4>No Products to display.</h4>
+      )}
+    </div>
+  );
+};
 
 export default ProductList;
