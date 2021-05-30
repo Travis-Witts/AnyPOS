845365f24855841b2ce6976c221438297a2251ac
Travis297 <74891603+Travis297@users.noreply.github.com>
2021-05-31 01:41:20 +0930

    :art: css: changed styling of product card components :art:

--- client/src/components/ProductCard/index.tsx
+++ client/src/components/ProductCard/index.tsx
@@ -1,34 +1,65 @@
-import React from 'react';
+/* eslint-disable jsx-a11y/anchor-is-valid */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React, { useRef, useState } from 'react';
 import './style.scss';
 
 type ICardProps = {
-  id: number | undefined;
+  name: string | undefined;
+  price: number | undefined;
+  quantity: number;
+  product_id: string | undefined;
 };
 
-const ProductCard: React.FC<ICardProps> = ({ id }: ICardProps) => {
-  const a = 1;
+const ProductCard: React.FC<ICardProps> = ({
+  name,
+  price,
+  quantity,
+  product_id,
+}: ICardProps) => {
+  const quantityRef = useRef<HTMLParagraphElement>(null);
+  const [quantityState, setQuantity] = useState<number>(quantity);
+
+  const addHandler = (event: React.MouseEvent) => {
+    const newQ = quantityState + 1;
+    setQuantity(newQ);
+  };
+
+  const removeHandler = (event: React.MouseEvent) => {
+    const newQ = quantityState - 1;
+    setQuantity(newQ);
+  };
 
   return (
-    <div className="">
-    <div className="card">
-      <header className="card-header">
-        <p className="card-header-title">Component {id}</p>
-      </header>
-      <div className="card-content">
-        <div className="content">
-          <p>Price: 100</p>
-          <p>Quantity: 2</p>
+    <div id={product_id}>
+      <div className="card has-background-grey-light">
+        <header className="card-header">
+          <p className="card-header-title">{name}</p>
+        </header>
+        <div className="card-content">
+          <div className="content">
+            <p>Price: ${price}.00</p>
+            <p>
+              Quantity: <span ref={quantityRef}>{quantityState}</span>
+            </p>
+          </div>
         </div>
+        <footer className="card-footer">
+          <a
+            onClick={addHandler}
+            className="card-footer-item button is-success"
+          >
+            Add
+          </a>
+          <a
+            onClick={removeHandler}
+            className="card-footer-item button is-danger"
+          >
+            Remove
+          </a>
+        </footer>
       </div>
-      <footer className="card-footer">
-        <a href=" " className="card-footer-item">
-          Add
-        </a>
-        <a href=" " className="card-footer-item">
-          Remove
-        </a>
-      </footer>
-    </div>
     </div>
   );
 };
