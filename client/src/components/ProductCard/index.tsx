17280661def54909187ff8515591ef73052be684
Travis297 <travis.witts@outlook.com>
2021-05-30 14:47:40 +0930

    :construction: feat: added product card component :construction:

---
+++ client/src/components/ProductCard/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style.scss';
+
+const ProductCard: React.FC = () => {
+    const a = 1
+
+    return (
+        <div className="product-card">
+            <div />
+        </div>
+    )
+}
+
+export default ProductCard;
