d50edac7fcb9b3e084a2598fe1ac0d03507c6b85
Travis297 <74891603+Travis297@users.noreply.github.com>
2021-05-31 00:08:59 +0930

    :construction: feat: added sale list component :construction:

---
+++ client/src/components/SaleList/index.tsx
@@ -0,0 +1,52 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect, useContext, Dispatch, SetStateAction } from 'react';
+import './style.scss';
+import axios from 'axios';
+import EditProduct from '../EditProduct';
+import ProductContext from '../../utils/ProductContext';
+
+type IProduct = {
+  name: string | undefined;
+  price: number | undefined;
+  quantity: number | undefined;
+  product_id: string | undefined;
+};
+
+type StoreModel = {
+  productsState: IProduct[] | [];
+  setProducts: Dispatch<SetStateAction<never[]>>;
+}
+
+const SaleList: React.FC = () => {
+  const {productsState, setProducts} = useContext<StoreModel>(ProductContext);
+
+  const getProducts = async () => {
+    const products = await axios.get('/product');
+    setProducts(products.data);
+  };
+
+    
+  useEffect(() => {
+    void getProducts();
+  }, []);
+  return (
+    <div>
+      {productsState.length ? (
+          <ul className="list-group">
+            {productsState.map((product: IProduct) => (
+              <EditProduct
+              product_id={product.product_id}
+                name={product.name}
+                price={product.price}
+                quantity={product.quantity}
+              />
+            ))}
+          </ul>
+      ) : (
+        <h4>No Products to display.</h4>
+      )}
+    </div>
+  );
+};
+
+export default SaleList;
