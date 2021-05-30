01ca85646d5c6dece90f88d03f7621a1d5299e03
Travis297 <74891603+Travis297@users.noreply.github.com>
2021-05-31 01:41:56 +0930

    :wrench: context: added sale context state :wrench:

---
+++ client/src/utils/SaleContext.ts
@@ -0,0 +1,24 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import React , { Dispatch, SetStateAction } from "react";
+
+
+type IProduct = {
+    name: string | undefined;
+    price: number | undefined;
+    quantity: number | undefined;
+    product_id: string | undefined;
+  };
+
+type StoreModel = {
+    saleState: IProduct[] | [];
+    setProducts: Dispatch<SetStateAction<never[]>>;
+}
+
+
+const SaleContext = React.createContext<StoreModel>({
+    saleState: [],
+    setProducts: () => {},
+  });
+
+
+export default SaleContext;
