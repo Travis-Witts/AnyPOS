2b7f52db5a978c70330ca462d28d02ac438511a7
Travis297 <travis.witts@outlook.com>
2021-05-30 15:49:21 +0930

    :construction: feat: added product card component :construction:

--- client/src/components/ProductCard/index.tsx
+++ client/src/components/ProductCard/index.tsx
@@ -1,14 +1,34 @@
 import React from 'react';
 import './style.scss';
 
-const ProductCard: React.FC = () => {
-    const a = 1
+type ICardProps = {
+  id: number | undefined;
+};
 
-    return (
-        <div className="product-card">
-            <div />
+const ProductCard: React.FC<ICardProps> = ({ id }: ICardProps) => {
+  const a = 1;
+
+  return (
+    <div className="card">
+      <header className="card-header">
+        <p className="card-header-title">Component {id}</p>
+      </header>
+      <div className="card-content">
+        <div className="content">
+          <p>Price: 100</p>
+          <p>Quantity: 2</p>
         </div>
-    )
-}
+      </div>
+      <footer className="card-footer">
+        <a href=" " className="card-footer-item">
+          Add
+        </a>
+        <a href=" " className="card-footer-item">
+          Remove
+        </a>
+      </footer>
+    </div>
+  );
+};
 
 export default ProductCard;
