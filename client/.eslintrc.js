a137b835d13f5d079bc8b63bb8e4f300ce911fc8
Travis297 <74891603+Travis297@users.noreply.github.com>
2021-06-02 01:32:18 +0930

    :wrench: config: editing linting rules

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -35,6 +35,7 @@ module.exports = {
     '@typescript-eslint/no-unsafe-assignment' : 'off',
     '@typescript-eslint/no-unsafe-member-access' : 'off',
     'react-hooks/exhaustive-deps' : 'off',
+    '@typescript-eslint/naming-convention' : 'off',
     '@typescript-eslint/no-unsafe-call' : 'off',
     '@typescript-eslint/no-explicit-any' : 'off',
     'no-void': 'off',
