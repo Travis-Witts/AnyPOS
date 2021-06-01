658a17f035a37e427cdb1250137ee93b95278814
Travis297 <74891603+Travis297@users.noreply.github.com>
2021-06-02 02:41:16 +0930

    :wrench: config: serving index.html to frontend :wrench:

--- routes/index.js
+++ routes/index.js
@@ -11,8 +11,4 @@ router.use('/transaction', transactionRoutes);
 router.use('/user', userRoutes);
 
 
-router.use((req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
-  });
-
 module.exports = router;
