d14f8277508f8b2cf082355defb7e61a418ad6f9
Travis297 <74891603+Travis297@users.noreply.github.com>
2021-06-07 22:51:59 +0930

    :recycle: chore: edited moved assets to pages for better organisation :recycle:

--- client/src/components/ReceiptPage/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const ReceiptPage: React.FC = () => {
-  const a = 1;
-
-  return <div />;
-};
-
-export default ReceiptPage;
--- client/src/components/Receipts/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import './style.scss';
-
-const Receipts: React.FC = () => (
-    <div />
-);
-export default Receipts;
